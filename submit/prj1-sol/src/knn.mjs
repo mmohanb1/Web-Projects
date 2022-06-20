0b7c5cba04ab535b01e24fea2fb93cac6e201bb2
Murali Mohan Bugata <mmohanb1@CS444-mmohanb1.bu.binghamton.edu>
2022-06-20 15:36:56 +0000

    completed code

--- submit/prj1-sol/src/knn.mjs
+++ submit/prj1-sol/src/knn.mjs
@@ -10,5 +10,64 @@ import { ok, err } from 'cs544-js-utils';
  *             different from length of subject testFeatures.
  */
 export default function  knn(testFeatures, trainLabeledFeatures, k=3) {
-  return err('knn() not implemented', { code: 'NO_IMPL' });
+//console.log('trainLabeledFeatures[0].label[0] : '+(typeof trainLabeledFeatures[0].label[0]));
+
+var distLabelIndexes = new Array();
+var dist = 0;
+for(var i=0; i<trainLabeledFeatures.length; i++)
+{
+	if(trainLabeledFeatures[i].features.length != testFeatures.length){
+//	console.log('Test and Training image # of bytes dont match');
+	    return err('Test and Training image # of bytes dont match', { code: 'BAD_FMT' });
+	    }
+
+	for(var j=0; j<trainLabeledFeatures[i].features.length; j++)
+          dist = dist + Math.pow((trainLabeledFeatures[i].features[j]-testFeatures[j]), 2);
+
+//const obj = {'d': dist, 'l': trainLabeledFeatures[i].label, 'i': i};
+
+const obj = {dist: dist, label: trainLabeledFeatures[i].label[0], index: i};
+//console.log('trainLabeledFeatures[i].label = '+new Label(trainLabeledFeatures[i].label));
+//const obj = [dist,trainLabeledFeatures[i].label,i];
+//console.log('obj.dist = '+obj.d+',obj.label = '+obj.l+', obj.index = '+obj.i);
+//distLabelIndexes = distLabelIndexes.concat(obj);
+distLabelIndexes.push(obj);
+dist = 0;
+}
+
+
+distLabelIndexes = distLabelIndexes.sort((a,b) => a.dist-b.dist).slice(0,k);
+//distLabelIndexes = distLabelIndexes.sort((a,b) => a[0]-b[0]).slice(0,k);
+//Array.from(distLabelIndexes, (obj) => console.log('obj.dist = '+obj.d+',obj.label = '+obj.l+', obj.index = '+obj.i));
+
+const map = new Map();
+//var maxCount=0, maxLabel , maxIndex=0;
+var maxCount=1, maxLabel=distLabelIndexes[0].label, maxIndex=0;
+map.set(distLabelIndexes[0].label,1);
+//for(var i=0; i<distLabelIndexes.length; i++)
+for(var i=1; i<distLabelIndexes.length; i++)
+{
+  const la = distLabelIndexes[i].label;
+//  const la = distLabelIndexes[i][1];
+   if(map.has(la))
+   {
+     const c = map.get(la);
+//console.log('*************la in map , la = '+la);
+   if(maxCount < c+1)
+   {
+     maxCount = c+1;
+     maxIndex = distLabelIndexes[i].index;
+//     maxIndex = distLabelIndexes[i][2];
+     maxLabel = la;
+   }
+      map.set(la, c+1);
+   }
+   else
+    map.set(la, 1);
+}
+//console.log('****maxIndex:'+maxIndex+', maxLabel.length : '+maxLabel.length);
+//console.log('****maxIndex:'+maxIndex+', maxLabel :'+maxLabel+'abc');
+//const arr = [maxLabel,maxIndex];
+const arr = new Array(maxLabel,maxIndex);
+return {hasErrors: false, val: arr};
 }
--- submit/prj1-sol/src/parse-images.mjs
+++ submit/prj1-sol/src/parse-images.mjs
@@ -12,6 +12,63 @@ import { ok, err } from 'cs544-js-utils';
  *             or # of images not equal to # of labels.
  */
 export default function parseImages(imageSpecs, imageBytes) {
-  return err('parseImages() not implemented', { code: 'NO_IMPL' });
-}
+	const imageSpecImagesObj = imageSpecs.images;
+	const imageSpecLabelsObj = imageSpecs.labels;
+//	var errObj = {anyError: false, type: err};
+//console.log('In parseImages');
+		const headerMagicN = imageSpecImagesObj[0].value;
+		const headerNImages = imageSpecImagesObj[1].value;
+		const headerNRows = imageSpecImagesObj[2].value;
+		const headerNCols = imageSpecImagesObj[3].value;
+		const headerLabelMagicN = imageSpecLabelsObj[0].value;
+//console.log('headerMagicN --> '+headerMagicN);
+	const view = new DataView(imageBytes.images.buffer);
 
+//console.log('images view -> '+view.byteLength);
+	 if (view.getInt32(0) !== headerMagicN || view.getInt32(8) !== headerNRows || view.getInt32(12) !== headerNCols)
+	   return err('***bad data in images detected!***', {code : 'BAD_VAL'});
+	  //errObj = {anyError: true, type: err('***bad data in images detected!***', {code : 'BAD_VAL'})};
+	   const imageBytesNImages = view.getInt32(4);
+        	const view1 = new DataView(imageBytes.labels.buffer);
+//console.log('labels view -> '+view1.byteLength);
+		if (view1.getInt32(0) !== headerLabelMagicN)
+	  	   return err('***bad data in labels detected***',{code : 'BAD_VAL'});
+//	  errObj = {anyError: true, type: err('***bad data in labels detected!***', {code : 'BAD_VAL'})};
+
+       	   const imageBytesNLabels = view1.getInt32(4);
+//	console.log('imageBytesNLabels = '+imageBytesNLabels);
+//	console.log('headerNRows = '+headerNRows);
+//		console.log('imageBytesNImages = '+imageBytesNImages);
+//		console.log('headerNCols = '+headerNCols);
+//		console.log('imageBytes.images.length = '+imageBytes.images.length);
+//		console.log('imageBytes.labels.length = '+imageBytes.labels.length);
+	    if(imageBytesNImages !== imageBytesNLabels || (imageBytesNImages*headerNRows*headerNCols+16 !== imageBytes.images.length)
+	    || (imageBytes.labels.length-8) !== imageBytesNLabels ||
+	    ((imageBytes.images.length-16)/(headerNRows*headerNCols) != imageBytes.labels.length-8))
+	    		 return err('detected inconstency in data',{code : 'BAD_FMT'});
+		//errObj = {anyError: true, type: err('***detected inconstency in data!***', {code : 'BAD_FMT'})};
+
+	   var labeledFeaturesFeatures = new Array() ;
+   	   var labeledFeaturesLabel = new Array();
+
+//	   for(var i=16; i<=imageBytes.images.length-4; i=i+4)
+	   for(var i=16; i<=imageBytes.images.length-(headerNRows*headerNCols); i=i+(headerNRows*headerNCols))
+//		   labeledFeaturesFeatures = labeledFeaturesFeatures.concat(imageBytes.images.slice(i, i+(headerNRows*headerNCols)));
+                   labeledFeaturesFeatures.push(imageBytes.images.slice(i, i+headerNRows*headerNCols));
+	   for(var i=8; i<=imageBytes.labels.length-1; i++){
+//	       	   labeledFeaturesLabel = labeledFeaturesLabel.concat(imageBytes.labels.slice(i, i+1).toString());
+	       	   labeledFeaturesLabel.push(imageBytes.labels.slice(i, i+1)[0].toString());
+		//   console.log('imageBytes.labels.slice(i, i+1)[0] = '+imageBytes.labels.slice(i, i+1)[0]);
+		   }
+//console.log('labeledFeaturesFeatures.length = '+labeledFeaturesFeatures.length);
+//console.log('labeledFeaturesLabel.length = '+labeledFeaturesLabel.length);
+//	   var labeledFeatures = [labeledFeaturesFeatures, labeledFeaturesLabel];
+	   var labeledFeatures = new Array();
+	   for(var i=0; i<labeledFeaturesFeatures.length; i++)
+		labeledFeatures.push({features: labeledFeaturesFeatures[i], label: labeledFeaturesLabel[i]});
+
+//console.log('labeledFeatures.length = '+labeledFeatures.length);
+return ok({hasErrors: false, val: labeledFeatures}.val);
+//return errObj.anyError ? errObj.type : ok({hasErrors: false, val: labeledFeatures}.val);
+
+}
