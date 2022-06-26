1348fad52c8a2afbb3b1852958657f7d6ef1b89c
Murali Mohan Bugata <mmohanb1@CS444-mmohanb1.bu.binghamton.edu>
2022-06-26 14:44:54 +0000

    started project 2

---
+++ submit/prj2-sol/src/features-dao.mjs
@@ -0,0 +1,11 @@
+import { MongoClient } from 'mongodb';
+
+import { ok, err } from 'cs544-js-utils';
+
+import { b64ToUint8Array, uint8ArrayToB64 } from './uint8array-b64.mjs';
+
+export default async function makeFeaturesDao(dbUrl) {
+  return err('unimplemented makeFeaturesDao()', { code: 'UNIMP' });
+}
+
+
