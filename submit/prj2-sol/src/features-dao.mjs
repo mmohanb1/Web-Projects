f1a3be756740fff9fcc48a1d1f72ec496d7d5af4
Murali Mohan Bugata <mmohanb1@CS444-mmohanb1.bu.binghamton.edu>
2022-06-27 15:18:33 +0000

    started working on code

--- submit/prj2-sol/src/features-dao.mjs
+++ submit/prj2-sol/src/features-dao.mjs
@@ -4,8 +4,43 @@ import { ok, err } from 'cs544-js-utils';
 
 import { b64ToUint8Array, uint8ArrayToB64 } from './uint8array-b64.mjs';
 
+
 export default async function makeFeaturesDao(dbUrl) {
-  return err('unimplemented makeFeaturesDao()', { code: 'UNIMP' });
+
+ class FeaturesDao{
+ //static c = 0;
+ static async setup(){
+  //source to below code : w3schools.com
+  console.log('in setup, url = '+dbUrl);
+  //MongoClient = require('mongodb').MongoClient;
+  console.log('in makeFeaturesDao');
+  MongoClient.connect(dbUrl, function(err, db) {
+  if (err)
+  {
+	//handle error here
+	console.log('error');
+  }
+  console.log("Database created!");
+    }
+  )
+
+    return new FeaturesDao();
+
+ }//close of func setup
+
+async close(){
+MongoClient.close();
+console.log('connection closed.');
+
+}	// close of function close()
+} //close of class
+//console.log('In makeFeaturesDao');
+//  return err('unimplemented makeFeaturesDao()', { code: 'UNIMP' });
+//setup();
+const obj = await FeaturesDao.setup();
+console.log('after calling setup');
+return {hasErrors: false, val: obj};
 }
+
 
 
