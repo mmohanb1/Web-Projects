755fb02f930ea3192f64f446c0a1ce8bf136d2fc
Murali Mohan Bugata <mmohanb1@CS444-mmohanb1.bu.binghamton.edu>
2022-06-28 09:44:18 +0000

    some progress on add

--- submit/prj2-sol/src/features-dao.mjs
+++ submit/prj2-sol/src/features-dao.mjs
@@ -6,65 +6,81 @@ import { b64ToUint8Array, uint8ArrayToB64 } from './uint8array-b64.mjs';
 
 
 export default async function makeFeaturesDao(dbUrl) {
+return FeaturesDao.make(dbUrl);
+}
 
- class FeaturesDao{
- //static c = 0;
- static async setup(){
-  //source to below code : w3schools.com
-  console.log('in setup, url = '+dbUrl);
-  //MongoClient = require('mongodb').MongoClient;
-  console.log('in makeFeaturesDao');
-  MongoClient.connect(dbUrl, function(err, db) {
-  if (err)
-  {
-	//handle error here
-	console.log('error');
-  }
-  console.log("Database created!");
+class FeaturesDao{
+constructor(params) {  Object.assign(this, params); }
+
+static async make(dbUrl) {
+ const params = {};
+    try {
+      params._client = new MongoClient(dbUrl);
+      await params._client.connect();
+      const db = params._client.db();
+      const features = db.collection(FEATURES_COLLECTION);
+      params.features = features;
+      await features.createIndex('featureId');
+      params.count = await features.countDocuments();
+      console.log('connected to db');
+      return ok(new FeaturesDao(params));
     }
-  )
+    catch (error) {
+      return err(error.message, { code: 'DB' });
+    }
+    
+ }//close of func make
 
-    return new FeaturesDao();
-
- }//close of func setup
-
-async close(obj){
-MongoClient.close();
-console.log('connection closed.');
-
+async close(){
+try{
+await this._client.close();
+}
+catch(e){
+err(error.message, { code: 'DB' });
+}
 }	// close of function close()
 
-async add(features, label)
+async add(features, isB64, label='')
 {
 console.log('In add****');
 console.log('features => '+features);
-const b64 = uint8ArrayToB64(features);
+const b64 = isB64 ? features : uint8ArrayToB64(features);
 console.log('b64 => '+b64);
-const generator = await idGenerator();
-console.log('generator => '+generator);
-const featuresId = 'featuresData_'+generator.next().value;
-console.log('featuresId => '+featuresId);
-const obj = {id: featuresId, data: b64, label: label};
-  console.log('obj => '+obj);
-  db.featuresCollection.insertOne(obj);
 
-  return {hasErrors: false, val: featuresId};
+const featureId = await this.nextFeatureId();
+console.log('featureId => '+featureId);
+
+const obj = {_id: featureId, features: b64, label: label};
+try {
+      const collection = this.features;
+      const insertResult = await collection.insertOne(obj);
+      const id1 = insertResult.insertedId;
+      if (id1 !== featureId) {
+	const msg = `expected inserted id '${id1}' to equal '${featureId}'`;
+	return err(msg, {code: 'DB'});
+      }
+    }
+    catch(e) {
+      return err(e.message, { code: 'DB' });
+    }
+    return ok({featureId});
 }//close of add func
 
-async * idGenerator(){
-let i = 1;
-while(true)
-   yield i++;
-}//end of idgenerator
+async nextFeatureId() {
+    const query = { _id: NEXT_ID_KEY };
+    const update = { $inc: { [NEXT_ID_KEY]: 1 } };
+    const options = { upsert: true, returnDocument: 'after' };
+    const ret =  await this.features.findOneAndUpdate(query, update, options);
+    const seq = ret.value[NEXT_ID_KEY];
+    return String(seq) + Math.random().toFixed(RAND_LEN).replace(/^0\./, '_');
+  }
 
 } //close of class
-//console.log('In makeFeaturesDao');
-//  return err('unimplemented makeFeaturesDao()', { code: 'UNIMP' });
-//setup();
-const obj = await FeaturesDao.setup();
-console.log('after calling setup');
-return {hasErrors: false, val: obj};
-}
+
+const FEATURES_COLLECTION = 'features';
+const NEXT_ID_KEY = 'count';
+const RAND_LEN = 2;
+
 
 
 
